e16daf2b6bb565f340d0f5ae995cb07736d1b6f1
Gavlovich Maksim <reverff@gmail.com>
2014-09-10 22:50:43 +0300

    done

--- work-1-4-console/src/main/Main.java
+++ work-1-4-console/src/main/Main.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 
+import com.core.GetArraySort;
+
 public class Main {
 
 	private static Scanner scanner;
@@ -16,16 +18,33 @@ public class Main {
 		int len = scanner.nextInt();
 		int[] array = new int[len];
 		
+		System.out.println("=========================================");
 		for (int i = 0; i<array.length; i++) {
 			System.out.println("\nSet the array element #" + (i + 1) + ": ");
 			array[i] = scanner.nextInt();
 		}
 
-		System.out.println();
+		System.out.println("=========================================\nYour array:");
 		for (int i = 0; i<array.length; i++)
 			System.out.print("|" + array[i] + "|");
+		System.out.println();
 		
+
 		
+		int [] sortedArray = GetArraySort.quickMethodReverse(array);
+		
+//		Other methods:		
+//		GetArraySort.bubbleMethod(array); 
+//		GetArraySort.bubbleMethodReverse(array);
+//		GetArraySort.insertMethod(array);
+//		GetArraySort.insertMethodReverse(array);
+//		GetArraySort.quickMethod(array);
+//		GetArraySort.selectMethod(array);
+//		GetArraySort.selectMethodReverse(array);
+		
+		System.out.println("=========================================\nSorted array:");
+		for (int i = 0; i<sortedArray.length; i++)
+			System.out.print("|" + sortedArray[i] + "|");
 		
 	}
 
