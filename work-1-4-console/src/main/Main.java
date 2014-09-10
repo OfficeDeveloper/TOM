1b817294f13e4f503c5cad2e052195660548c288
Gavlovich Maksim <reverff@gmail.com>
2014-09-10 22:26:18 +0300

    1

---
+++ work-1-4-console/src/main/Main.java
@@ -0,0 +1,30 @@
+package main;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Main {
+
+	private static Scanner scanner;
+
+	public static void main(String[] args) throws IOException {
+
+		System.out.println("Set the array length: ");
+		
+		scanner = new Scanner(new InputStreamReader(System.in));
+		int len = scanner.nextInt();
+		int[] array = new int[len];
+		
+		for (int i = 0; i<array.length; i++) {
+			System.out.println("\nSet the array element #" + i + ": ");
+			array[i] = scanner.nextInt();
+		}
+
+		System.out.println();
+		for (int i = 0; i<array.length; i++)
+			System.out.print("|" + array[i] + "|");
+		
+	}
+
+}
